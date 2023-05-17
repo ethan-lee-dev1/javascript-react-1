8c791201bd62e63919c31711d17b259c3c3a548b
Ethan Lee <elee@xpanxion.com>
2023-05-17 17:23:19 -0400

    Finish ex3

--- src/Student4/Ex3/Main.js
+++ src/Student4/Ex3/Main.js
@@ -1,10 +1,14 @@
-import React from 'react'
-import './Main.css'
+import React from "react";
+import "./Main.css";
+import { Tweet } from "./Tweet";
 
 export const Main = () => {
   return (
-    <div>
-        Hello I'm student id: 1.
+    <div className="Tweets">
+      <div>Tweet Storm</div>
+      <Tweet message="It's payday!!!" color="green" />
+      <Tweet message="I just bought a new car!" color="yellow" />
+      <Tweet message="I just smashed my new car." color="red" />
     </div>
-  )
-}
+  );
+};
