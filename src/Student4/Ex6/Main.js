d14d643c4cdbcd3e9ed0673834aa3956d2463d23
Ethan Lee <elee@xpanxion.com>
2023-05-24 12:12:27 -0400

    Finish ex6

--- src/Student4/Ex6/Main.js
+++ src/Student4/Ex6/Main.js
@@ -1,10 +1,32 @@
-import React from 'react'
-import './Main.css'
+import React, { useState } from "react";
+import "./Main.css";
+import { useRef } from "react";
 
 export const Main = () => {
+  let refInput = useRef(null);
+  let [sentence, setSentence] = useState("");
+
+  const addWord = () => {
+    console.log(
+      "🚀 ~ file: Main.js:15 ~ addWord ~ refInput.current.value:",
+      refInput.current.value
+    );
+
+    setSentence(sentence + " " + refInput.current.value);
+    refInput.current.value = "";
+  };
+
+  const clearWord = () => {
+    setSentence("");
+  };
+
   return (
     <div>
-        Hello I'm student id: 1.
+      <h1>Sentence Builder</h1>
+      <p>Sentence: {sentence}</p>
+      <input ref={refInput} type="text"></input>
+      <button onClick={() => addWord()}>Add Word</button>
+      <button onClick={() => clearWord()}>Reset</button>
     </div>
-  )
-}
+  );
+};
